fdb56a9237e367c732804c63f5516b6522d901d9
Harshit Luthra <love.coolsachin@gmail.com>
2024-04-25 16:44:52 +0530

    docs(README.md): update NodeJs version to 20.12.2 and add installation instructions feat(index.js): refactor routes to use arrow functions for simplicity and consistency feat(index.js): add new route '/load/:load' to generate CPU load with crypto operations feat(package.json): update fastify dependency to version 4.26.2

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-var fastify = require('fastify')({ logger: true });
+var fastify = require("fastify")({ logger: true });
+const crypto = require("crypto");
 
 function randomIntFromInterval(min, max) {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-/* GET home page. */
-fastify.get('/', function (req, res, next) {
-  return { success: true };
-});
+function generateLoad(numOperations) {
+  const promises = [];
 
-fastify.get('/definedLatency/:latency', async function (req, res, next) {
+  // Create an array of Promises to perform some computation
+  for (let i = 0; i < numOperations; i++) {
+    promises.push(
+      new Promise((resolve) => {
+        // Simulate CPU-heavy task with crypto operation
+        crypto.pbkdf2("password", "salt", 100000, 64, "sha512", () => {
+          resolve();
+        });
+      }),
+    );
+  }
+
+  // Return a Promise that resolves when all operations are complete
+  return Promise.all(promises);
+}
+
+/* GET home page. */
+fastify.get("/", (_req, _res, _next) => ({ success: true }));
+
+fastify.get("/definedLatency/:latency", async function (req, _res, _next) {
   const latency = req.params.latency;
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   await delay(parseInt(latency)); /// waiting 1 second.
   return { success: true, latency: latency };
 });
 
-fastify.get('/randomLatency', async function (req, res, next) {
+fastify.get("/randomLatency", async function (_req, _res, _next) {
   const min = 200;
   const max = 5000;
   const rndInt = randomIntFromInterval(min, max);
@@ -25,8 +43,12 @@ fastify.get('/randomLatency', async function (req, res, next) {
   await delay(parseInt(rndInt)); /// waiting 1 second.
   return { success: true, latency: rndInt };
 });
+fastify.get("/load/:load", async function (req, _res, _next) {
+  generateLoad(parseInt(req.params.load)).then();
+  return { success: true, load: req.params.load };
+});
 
-fastify.listen(3000, '0.0.0.0', function (err, address) {
+fastify.listen(3000, "0.0.0.0", function (err, _address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
